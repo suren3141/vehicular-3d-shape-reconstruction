19ccc3a70a3b4a2a6fbc22a913c3870652e55ce7
Ubuntu <ubuntu@ssritharan-p3.img.astrazeneca.net>
2023-07-18 23:52:49 +0000

    update links

--- BAAM/tools/download_data.sh
+++ BAAM/tools/download_data.sh
@@ -1,19 +1,18 @@
 # Path: BAAM\tools\download_data.sh
 
-symlink = https://syncandshare.lrz.de/getlink/fiQrkszdkDasiL3jvuGD3o/symlink.py
-annot_files = https://syncandshare.lrz.de/getlink/fiXqyA4765oTpHRBHYEey9/apollo.zip
-train_files = https://ad-apolloscape.cdn.bcebos.com/3d-car-understanding-train.tar.gz
-test_files = https://ad-apolloscape.cdn.bcebos.com/3d-car-understanding-test.tar.gz
+symlink="https://syncandshare.lrz.de/dl/fiQrkszdkDasiL3jvuGD3o/symlink.py"
+annot_files="https://syncandshare.lrz.de/dl/fiXqyA4765oTpHRBHYEey9/apollo.zip"
+train_files="https://ad-apolloscape.cdn.bcebos.com/3d-car-understanding-train.tar.gz"
+test_files="https://ad-apolloscape.cdn.bcebos.com/3d-car-understanding-test.tar.gz"
 
-cd BAAM
+out_dir="$1"
 
-out_dir = $1
-
+echo $out_dir
 cd $out_dir
 
 wget -O symlink.py $symlink
 wget -O apollo.zip $annot_files
-wget -O 3d-car-understanding-train.tar.gz $train_files
+#wget -O 3d-car-understanding-train.tar.gz $train_files
 wget -O 3d-car-understanding-test.tar.gz $test_files
 
 tar -xvf 3d-car-understanding-train.tar.gz
