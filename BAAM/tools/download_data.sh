f5ca1bc14a5a60b680f0206a19f582e70789a381
Ubuntu <ubuntu@ssritharan-p3.img.astrazeneca.net>
2023-07-19 00:02:27 +0000

    added symlink

--- BAAM/tools/download_data.sh
+++ BAAM/tools/download_data.sh
@@ -1,18 +1,21 @@
 # Path: BAAM\tools\download_data.sh
 
-symlink="https://syncandshare.lrz.de/dl/fiQrkszdkDasiL3jvuGD3o/symlink.py"
 annot_files="https://syncandshare.lrz.de/dl/fiXqyA4765oTpHRBHYEey9/apollo.zip"
 train_files="https://ad-apolloscape.cdn.bcebos.com/3d-car-understanding-train.tar.gz"
 test_files="https://ad-apolloscape.cdn.bcebos.com/3d-car-understanding-test.tar.gz"
 
-out_dir="$1"
+OUTDIR="$1"
+BASEDIR=$(dirname "$0")
 
-echo $out_dir
-cd $out_dir
 
-wget -O symlink.py $symlink
+cp "$BASEDIR/symlink.py" "$OUTDIR/symlink.py"
+
+
+echo $OUTDIR
+cd $OUTDIR
+
 wget -O apollo.zip $annot_files
-#wget -O 3d-car-understanding-train.tar.gz $train_files
+wget -O 3d-car-understanding-train.tar.gz $train_files
 wget -O 3d-car-understanding-test.tar.gz $test_files
 
 tar -xvf 3d-car-understanding-train.tar.gz
@@ -21,4 +24,3 @@ unzip apollo.zip -d BAAM
 
 python symlink.py --data_dir BAAM/train --src_dir train
 python symlink.py --data_dir BAAM/test --src_dir test
-
